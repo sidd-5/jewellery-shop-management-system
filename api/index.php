6b6ee72f50010f2869904a1c6c5ac8ad05f7dae2
Siddhesh chavan <siddhchavan1@gmail.com>
2025-07-31 16:52:15 +0530

    update api/index.php

--- api/index.php
+++ api/index.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>BhoomiJewels - Home</title>
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="http://localhost/jewellery-shop-management-system/style.css">
 </head>
 <body>
   <header>
